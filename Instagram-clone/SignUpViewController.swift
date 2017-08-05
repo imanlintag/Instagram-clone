30612fb8241f2ece07b4b3cdd815c42e2b3f8b72
[myblog] <imanlintag13@mittymonarch.com>
2017-08-05 01:11:37 -0700

    Created Sign In and Sign Up controllers

---
+++ Instagram-clone/SignUpViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SignUpViewController.swift
+//  Instagram-clone
+//
+//  Created by Iman Lintag on 8/5/17.
+//  Copyright © 2017 Iman Mallari Lintag. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
